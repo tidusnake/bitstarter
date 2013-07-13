e9bcb70a7cc9be9781cf86a47c2f119563a15cc1
tidusnake <jienanyao@gmail.com>
2013-07-13 23:14:21 +0000

    changed web.js

--- web.js
+++ web.js
@@ -2,8 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var readIndexHtml = function() {
+    var fs = require('fs');
+    return fs.readFileSync('index.html','utf8');
+}
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(readIndexHtml());
 });
 
 var port = process.env.PORT || 5000;
